97eefd359f7ee2b609e9e2df47ad343cc308f49d
Sibsankar Satpati <sibsankar17@gmail.com>
2017-07-25 19:35:40 +0530

    Setup SucessFully done

---
+++ OnlineShopingBackend/src/main/java/net/Sibsankar/OnlineShopingBackend/App.java
@@ -0,0 +1,13 @@
+package net.Sibsankar.OnlineShopingBackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
